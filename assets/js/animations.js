f29dd1049be446b63cdb00d2167c6a6d6ba24e33
Bhuvanesh <84269107+BhuvaneshKhangar@users.noreply.github.com>
2023-04-28 01:47:41 +0530

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,6 +1,7 @@
 // var historyContent = document.querySelector('#history-content');
 // var m3 = document.getElementsByClassName('.our-history');
 
+
 // document.addEventListener('scroll', () => {
 //     if (window.scrollY >= historyContent.getBoundingClientRect().top) {
 //         historyContent.classList.add('opacity');
